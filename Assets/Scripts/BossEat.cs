a285842f1a11a7e4fb0390e378ac4a003667b686
Bart Helleman <bart_helleman@hotmail.nl>
2017-10-31 15:47:07 +0100

    Added timer

--- Assets/Scripts/BossEat.cs
+++ Assets/Scripts/BossEat.cs
@@ -1,42 +1,56 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class BossEat : MonoBehaviour {
+public class BossEat : MonoBehaviour
+{
 
     [SerializeField] private AudioSource yumyum;
     [SerializeField] private int winScore = 5;
-    private int score;
     [SerializeField] private GameObject _uiVictory;
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    [SerializeField] private GameObject _loseText;
+    [SerializeField] private Text _timerText;
+    [SerializeField] private float _timer = 120f;
 
-	}
+    private int _score;
 
-    void OnTriggerEnter(Collider collision) {
-        eat(collision);
+    private void Update()
+    {
+        if (_timer <= 0)
+            Lose();
+
+        _timer -= Time.deltaTime;
+        _timerText.text = _timer.ToString();
     }
 
-    void eat(Collider collision) {
+    private void OnTriggerEnter(Collider collision)
+    {
+        Eat(collision);
+    }
+
+    private void Eat(Collider collision)
+    {
         var pickable = collision.GetComponent<Pickable>();
         if (!pickable) return;
         if (pickable.Type != Pickable.PickableType.PINK_MUSHROOM) return;
 
         yumyum.Play();
         Destroy(collision.gameObject);
-        score++;
-        if (score >= winScore) {
-            win();
+        _score++;
+        if (_score >= winScore)
+        {
+            Win();
         }
     }
 
-    void win() {
+    private void Win()
+    {
         _uiVictory.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    private void Lose()
+    {
+        _loseText.SetActive(true);
         Time.timeScale = 0f;
     }
 }
