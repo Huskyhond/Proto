88ae0a2968bb20e89cba73afdced74c802afc359
Bart Helleman <bart_helleman@hotmail.nl>
2017-10-31 14:59:24 +0100

    Incredible UI bar for watering can

--- Assets/Scripts/WateringCan.cs
+++ Assets/Scripts/WateringCan.cs
@@ -2,17 +2,23 @@
 
 public class WateringCan : MonoBehaviour
 {
+    [SerializeField] private RectTransform BackgroundBar;
+    [SerializeField] private RectTransform WaterBar;
+
     [SerializeField] private int MaxWaterAmount = 147; // Currently uses 49 water to fully grow a mushroom
     private int _waterAmount;
 
     private void Start()
     {
         _waterAmount = MaxWaterAmount;
+        BackgroundBar.sizeDelta = new Vector3(_waterAmount, BackgroundBar.sizeDelta.y);
+        WaterBar.sizeDelta = new Vector2(_waterAmount, WaterBar.sizeDelta.y);
     }
 
     public void FillWateringCan()
     {
         _waterAmount = MaxWaterAmount;
+        WaterBar.sizeDelta = new Vector2(_waterAmount, WaterBar.sizeDelta.y);
     }
 
     public int GetWaterAmount()
@@ -23,5 +29,6 @@ public class WateringCan : MonoBehaviour
     public void UseWater()
     {
         _waterAmount--;
+        WaterBar.sizeDelta = new Vector2(_waterAmount, WaterBar.sizeDelta.y);
     }
 }
